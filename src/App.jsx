3abb433c472ec9318aa1b07b80287475a14f970e
Rahul Bansal <42.rahulbansal@gmail.com>
2024-03-07 21:04:23 +0530

    Code cleanup and refractoring
    - Experience to tracks

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { BrowserRouter } from "react-router-dom"
-import {About, Contact, Feedbacks, Hero, Navbar, Experience,Tech,Works,StarsCanvas,Extrapage} from './components'
+import { BrowserRouter } from "react-router-dom";
+import {
+	About,
+	Contact,
+	Feedbacks,
+	Hero,
+	Navbar,
+	Tracks,
+	Tech,
+	Works,
+	StarsCanvas,
+	Extrapage,
+} from "./components";
 
 function App() {
- 
-
-  return (
-    <BrowserRouter>
-    <div className="relative z-0 h-auto bg-primary">
-      <div className="bg-hero-pattern h-auto bg-cover bg-no-repeat bg-center" >
-        <Navbar />
-        <Hero />
-      </div>
-      <About />
-      <Feedbacks />
-      <Experience />
-      {/* <Tech /> */}
-      {/* <Works /> */}
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-
-      </div>
-      
-      
-
-    </div>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 h-auto bg-primary">
+				<div className="bg-hero-pattern h-auto bg-cover bg-no-repeat bg-center">
+					<Navbar />
+					<Hero />
+				</div>
+				<About />
+				<Feedbacks />
+				<Tracks />
+				{/* <Tech /> */}
+				{/* <Works /> */}
+				<div className="relative z-0">
+					<Contact />
+					<StarsCanvas />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
-import Experience from './Experience';
+import Tracks from './Tracks';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
@@ -14,7 +14,7 @@ export {
   Navbar,
   About,
   Tech,
-  Experience,
+  Tracks,
   Works,
   Feedbacks,
   Contact,
