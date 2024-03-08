9313fed4f29ab6ea0012b99c47f97a8dd8010c94
Rahul Bansal <42.rahulbansal@gmail.com>
2024-03-08 14:40:19 +0530

    node package update

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { Computers } from "./canvas";
+
+
+const FAQ = () => {
+  return (
+	<div>FAQ</div>
+  )
+}
+
+export default FAQ
